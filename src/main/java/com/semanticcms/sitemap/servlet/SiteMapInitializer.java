5cebda4487928f1d855f7d594d74df66ffd36f08
AO Industries, Inc. <support@aoindustries.com>
2016-08-10 00:09:58 -0500

    Renaming project from ao-web-sitemap-servlet to semanticcms-sitemap-servlet.

--- src/main/java/com/semanticcms/sitemap/servlet/SiteMapInitializer.java
+++ src/main/java/com/semanticcms/sitemap/servlet/SiteMapInitializer.java
@@ -1,26 +1,26 @@
 /*
- * ao-web-sitemap-servlet - Automatic sitemaps for web page content in a Servlet environment.
+ * semanticcms-sitemap-servlet - Automatic sitemaps for web page content in a Servlet environment.
  * Copyright (C) 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of ao-web-sitemap-servlet.
+ * This file is part of semanticcms-sitemap-servlet.
  *
- * ao-web-sitemap-servlet is free software: you can redistribute it and/or modify
+ * semanticcms-sitemap-servlet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ao-web-sitemap-servlet is distributed in the hope that it will be useful,
+ * semanticcms-sitemap-servlet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-web-sitemap-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-sitemap-servlet.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.aoindustries.web.sitemap.servlet;
+package com.semanticcms.sitemap.servlet;
 
 import com.semanticcms.core.model.Book;
 import com.semanticcms.core.servlet.BooksContextListener;
